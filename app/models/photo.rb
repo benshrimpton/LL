3bd0a67cf5b0b467f4e01ca336cfabf4b6bf4ecf
Ben <benjaminshrimpton@yahoo.co.uk>
2015-07-09 10:58:23 -0400

    comment sot resources for file uploads

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,6 @@ class Photo < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://placehold.it/200x200"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   
-  
   belongs_to :album
   belongs_to :portfolio
   belongs_to :user
